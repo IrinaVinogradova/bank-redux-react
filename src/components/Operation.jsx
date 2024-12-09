a5367620f4335735ce66b7d5a9c0b38e1b2ad439
Irina <vinogradova8998@gmail.com>
2024-12-09 16:08:31 +0200

    add react-redux-tailwind

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,19 +1,26 @@
 import {useState} from "react";
 import {deposit, withDraw} from "../actions/accountActions.js";
 import {store} from "../configurStore/store.js";
+import {useDispatch} from "react-redux";
 
 
 const Operation = () => {
     const[sum, setSum] = useState(1);
+    const dispatch = useDispatch();
     return (
-        <div>
-            <button onClick={() => store.dispatch(withDraw(sum))}>Withdraw</button>
+        <div className={'flex justify-center'}>
+            <button
+                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg text-lg'}
+                    onClick={() => dispatch(withDraw(sum))}>Withdraw</button>
             <input
+                className={'text-center border rounded-lg'}
                 type={'number'}
                 onChange={e => setSum(+e.target.value)}
                 value={sum}
             />
-            <button onClick={() => store.dispatch(deposit(sum))}>Deposit</button>
+            <button
+                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg text-lg'}
+                onClick={() => dispatch(deposit(sum))}>Deposit</button>
         </div>
     );
 };
