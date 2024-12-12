03168c2313be7eb1a299a8454be5dfc8f218e374
Irina <vinogradova8998@gmail.com>
2024-12-12 21:17:27 +0200

    add enhancers

---
+++ src/enhancer/thunkEnhancer.js
@@ -0,0 +1,8 @@
+export  const thunkEnhancer = ({getState, dispatch}) => next => action => {
+    if(typeof action === 'function') {
+
+
+        return action(dispatch, getState);
+    }
+    return next(action);
+}
