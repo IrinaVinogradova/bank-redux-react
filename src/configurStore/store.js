a05ebecca2abbf6a74771df14d14e23db8edd4fd
Irina <vinogradova8998@gmail.com>
2024-12-12 21:45:24 +0200

    add enhancers

--- src/configurStore/store.js
+++ src/configurStore/store.js
@@ -3,10 +3,12 @@ import {accountReducer} from "../reducer/accountReduser.js";
 import {applyMiddleware, legacy_createStore as createStore} from "redux";
 import {loggerEnhancer} from "../enhancer/loggerEnhancer.js";
 import {thunkEnhancer} from "../enhancer/thunkEnhancer.js";
+import {thunk} from "redux-thunk";
+import logger from "redux-logger";
 
 const initialState = {
     balance: 0,
     quote: 'Winter is coming...'
 
 }
-export const store = createStore(accountReducer, initialState, applyMiddleware(thunkEnhancer, loggerEnhancer));
+export const store = createStore(accountReducer, initialState, applyMiddleware(thunk, logger));
